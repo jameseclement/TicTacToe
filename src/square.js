f122956048243c56856e01982bed44dacecb7409
James Clement <jameseclement@gmail.com>
2019-05-16 15:30:10 -0400

    add handleClick, setting the square to be the state of the marker

--- src/square.js
+++ src/square.js
@@ -1,10 +1,23 @@
 import React, { Component } from "react";
 
 class Square extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      value: ""
+    };
+  }
+
+  handleClick = marker => {
+    this.setState({
+      value: marker
+    });
+  };
   render() {
     return (
-      <button onClick={() => console.log("clicked")} className="square">
-        {this.props.value}
+      <button onClick={() => this.handleClick()} className="square">
+        {this.state.value}
       </button>
     );
   }
